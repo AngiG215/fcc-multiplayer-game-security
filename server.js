cc6d9ed020ce47445fbc4da0c45b1142c146d12a
Angela <angieadrigarcia572@gmail.com>
2026-02-20 23:19:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -25,14 +25,13 @@ app.use(helmet.xssFilter());
 app.use(helmet.noCache());
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
 
-// --- CONFIGURACIÓN ---
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/assets', express.static(process.cwd() + '/assets'));
-
+app.use(cors({origin: '*'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({origin: '*'})); 
+// --- CONFIGURACIÓN ---
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/assets', express.static(process.cwd() + '/assets'));
 
 // --- RUTAS ---
 app.route('/').get(function (req, res) {
